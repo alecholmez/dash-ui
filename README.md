d134c5015dba16f0ee7e5d5cb2bed6391a0d1e81
Alec Holmes <alecholmez@me.com>
2017-09-22 22:39:21 -0400

    alpha release

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 UI portion of Decipher's CircleCI dashboard
 
 ## Getting Started
+1. Clone repo:
+```
+
+```
+2. Fetch dependencies:
+```
+npm install
+```
+3. Run web server
 ```
 npm run start
 ```
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -16,45 +16,56 @@ class Cards extends React.Component {
           <div className="uk-child-width-1-5 uk-flex-center uk-grid-match" data-uk-grid>
               {this.props.builds.map((build,i) => (
                   <div key={i}>
-                      <div className={'uk-tile uk-tile-' + build.build_info.status +  ' uk-light uk-padding uk-animation-scale-up'}>
-                          <div data-uk-grid>
-                              <div className="uk-width-3-4">
-                                  <h3 className="uk-text-uppercase uk-display-inline-block uk-margin-remove-bottom">{build.reponame}</h3>
+                      <div className={'uk-tile uk-tile-' + build.build_info.status +  ' uk-padding-remove uk-animation-scale-up'}>
+
+                          <div className={'uk-tile-header-' + build.build_info.status + ' uk-light uk-padding-small'}>
+                              <div className="uk-margin-remove-bottom" data-uk-grid>
+                                  <div className="uk-width-3-4">
+                                      <h3 className="uk-text-uppercase uk-margin-remove-bottom">{build.reponame}</h3>
+                                      <p className="uk-margin-left uk-margin-remove-top uk-margin-remove-bottom uk-text-capitalize uk-margin-remove-left uk-text-small">{build.build_info.branch}</p>
+                                  </div>
+                              </div>
+                              <hr />
+                              <div>
+                                  <p className="uk-text-meta uk-display-inline-block uk-margin-remove-bottom">Time To Complete: </p>
+                                  <p className="uk-display-inline uk-margin-left uk-margin-remove-top uk-margin-remove-bottom uk-text-capitalize uk-text-small">{build.build_info.stop_time}</p>
                               </div>
                           </div>
-                          <hr />
-                          <div className="uk-margin-remove-top">
-                              <p className="uk-text-meta uk-display-inline-block">Lifecycle: </p>
-                              <p className="uk-display-inline-block uk-margin-left uk-margin-remove-top uk-margin-remove-bottom uk-float-right uk-text-capitalize">{build.build_info.lifecycle}</p>
-                          </div>
-                          <div className="uk-margin-remove-top">
-                              <p className="uk-text-meta uk-display-inline-block">Build Status: </p>
-                              <p className="uk-display-inline-block uk-margin-left uk-margin-remove-top uk-margin-remove-bottom uk-float-right uk-text-capitalize">{build.build_info.status}</p>
-                          </div>
-                          <div className="uk-margin-remove-top">
-                              <p className="uk-text-meta uk-display-inline-block">Build Number: </p>
-                              <p className="uk-display-inline-block uk-margin-left uk-margin-remove-top uk-margin-remove-bottom uk-float-right">{build.build_info.build_num}</p>
-                          </div>
-                          <div className="uk-margin-remove-top">
-                              <p className="uk-text-meta uk-display-inline-block">Branch: </p>
-                              <p className="uk-display-inline-block uk-margin-left uk-margin-remove-top uk-margin-remove-bottom uk-float-right uk-text-capitalize uk-margin-remove-left">{build.build_info.branch}</p>
-                          </div>
-                          <div className="uk-margin-remove-top">
-                              <p className="uk-text-meta uk-display-inline-block">Triggered By: </p>
-                              <img className="uk-border-circle uk-float-right uk-display-inline-block" src={build.build_info.user.avatar_url} width="40" height="40" alt="Missing Profile"/>
-                              <p className="uk-display-inline-block uk-margin-remove-top uk-margin-remove-bottom uk-float-right uk-margin-remove-left">{build.build_info.user.name}</p>
-                          </div>
-                          <hr />
-                          <div className="uk-margin-remove-bottom">
-                              <p className="uk-text-meta uk-display-inline-block uk-margin-remove-bottom">Subject: </p>
-                              <div className="uk-display-inline-block uk-margin-left uk-margin-remove-top uk-margin-remove-bottom uk-float-right subject-wrap">
-                                  <p className=" uk-margin-remove-bottom uk-margin-remove-top">{build.build_info.subject}</p>
+
+
+                          <div className="uk-padding-small">
+                              <div className="uk-dark">
+                                  <div className="uk-margin-remove-top">
+                                      <p className="uk-text-meta uk-display-inline-block">Lifecycle: </p>
+                                      <p className="uk-display-inline-block uk-margin-left uk-margin-remove-top uk-margin-remove-bottom uk-float-right uk-text-capitalize">{build.build_info.lifecycle}</p>
+                                  </div>
+                                  <div className="uk-margin-remove-top">
+                                      <p className="uk-text-meta uk-display-inline-block">Build Status: </p>
+                                      <p className="uk-display-inline-block uk-margin-left uk-margin-remove-top uk-margin-remove-bottom uk-float-right uk-text-capitalize">{build.build_info.status}</p>
+                                  </div>
+                                  <div className="uk-margin-remove-top">
+                                      <p className="uk-text-meta uk-display-inline-block">Build Number: </p>
+                                      <p className="uk-display-inline-block uk-margin-left uk-margin-remove-top uk-margin-remove-bottom uk-float-right">{build.build_info.build_num}</p>
+                                  </div>
+                                  <div className="uk-margin-remove-top">
+                                      <p className="uk-text-meta uk-display-inline-block uk-margin-remove-bottom">Triggered By: </p>
+                                      <img className="uk-border-circle uk-float-right uk-display-inline-block" src={build.build_info.user.avatar_url} width="40" height="40" alt="Missing Profile"/>
+                                      <p className="uk-display-inline-block uk-margin-remove-top uk-margin-remove-bottom uk-float-right uk-margin-remove-left">{build.build_info.user.name}</p>
+                                  </div>
+
+                                  <div className="uk-margin-remove-top">
+                                      <p className="uk-text-meta uk-display-inline-block">Subject: </p>
+                                      <div className="uk-display-inline-block uk-margin-left uk-margin-remove-top uk-float-right subject-wrap">
+                                          <p className="uk-margin-remove-top uk-text-small">{build.build_info.subject}</p>
+                                      </div>
+                                  </div>
+                                  <div className="">
+                                      <p className="uk-text-meta uk-display-inline-block uk-margin-remove-bottom">Language: </p>
+                                      <p className="uk-display-inline-block uk-margin-left uk-margin-remove-top uk-margin-remove-bottom uk-float-right">{build.language}</p>
+                                  </div>
                               </div>
                           </div>
-                          <div className="uk-margin-remove-bottom">
-                              <p className="uk-text-meta uk-display-inline-block uk-margin-remove-bottom">Language: </p>
-                              <p className="uk-display-inline-block uk-margin-left uk-margin-remove-top uk-margin-remove-bottom uk-float-right">{build.language}</p>
-                          </div>
+
                       </div>
                   </div>
               ))}
